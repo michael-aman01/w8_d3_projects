0042b97796ffeaedfd90f00f5ca8e808c4ae211b
Daniel Culbertson <daniel.j.culbertson@gmail.com>
2022-09-28 14:54:03 -0700

    finish addNumbers

--- callbackExercises.js
+++ callbackExercises.js
@@ -39,4 +39,35 @@ class Clock {
 
 
 
-let c = new Clock()
+// let c = new Clock()
+
+const readLine = require("readline");
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function whatever(arg) {
+    console.log("I am in whatever")
+    console.log(arg)
+}
+
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        let newNum = rl.question("Enter number please\n", (response) => {
+            let number = parseInt(response);
+            sum += number;
+            // console.log(`Total Sum: ${sum}`);
+            numsLeft -= 1;
+            rl.close();
+            addNumbers(sum, numsLeft, completionCallback);
+            
+            })  
+    }else{
+        completionCallback(sum);
+    }
+}
+
+addNumbers(0,3, whatever);
