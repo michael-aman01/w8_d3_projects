5fd80adb18fa7442b1fc815564ece9b8de2536cf
michael <michael_aman92@icloud.com>
2022-09-28 14:06:17 -0700

    complete add numbers

--- callbackExercises.js
+++ callbackExercises.js
@@ -4,20 +4,39 @@ class Clock {
         
         this.hours = this.dateObj.getUTCHours();
         this.minutes = this.dateObj.getUTCMinutes();
-        this.seconds = this.dateObj.getUTCSeconds();
 
-        this.display = `${this.hours}:${this.minutes}:${this.seconds}`
-        
-        
+        this.seconds = this.dateObj.getUTCSeconds();
+        setInterval(() => {
+            this._tick()
+        }, 1000);
+
+    }
+
+    printTime() {
+        console.clear()
+        console.log(`${this.hours}:${this.minutes}:${this.seconds}`)
     }
 
     _tick() {
+        this.printTime()
+        if(this.seconds + 1 === 60){
+            this.seconds = 0;
+            this.minutes += 1;
+        }else{
+            this.seconds += 1;
+        }
+        
+        if(this.minutes === 60){
+            this.minutes = 0;
+            this.hours += 1;
+        }
 
+        if(this.hours + 1 === 24){
+            this.hours = 0;
+        }
     }
-
-    printTime( {
-
-    })
 }
+
+
 
 let c = new Clock()
