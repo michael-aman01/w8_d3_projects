ee2f5f2ce878b20423ed0ff448dd16d59458ad7b
Daniel Culbertson <daniel.j.culbertson@gmail.com>
2022-09-28 17:31:47 -0700

    finish nothing

--- callbackExercises.js
+++ callbackExercises.js
@@ -108,18 +108,69 @@ Function.prototype.myThrottle = function(interval){
 }
 
 
-  class Neuron {
-    fire() {
-      console.log("Firing!");
-    }
-  }
+//   class Neuron {
+//     fire() {
+//       console.log("Firing!");
+//     }
+//   }
   
-  const neuron = new Neuron();
+//   const neuron = new Neuron();
 
   
-  neuron.fire = neuron.fire.myThrottle(500);
+//   neuron.fire = neuron.fire.myThrottle(500);
   
-  const interval = setInterval(() => {
-    neuron.fire();
-  }, 10);
+//   const interval = setInterval(() => {
+//     neuron.fire();
+//   }, 10);
   
+Function.prototype.myDebounce = function(interval) {
+    let methodCalled = false;
+    return (...args)=> {
+        if (methodCalled === false){
+            setInterval(()=> this(...args), interval);
+            methodCalled = true;
+        
+        }
+
+    }
+}
+
+class SearchBar {
+    constructor() {
+      this.query = "";
+  
+      this.type = this.type.bind(this);
+      this.search = this.search.bind(this);
+    }
+  
+    type(letter) {
+      this.query += letter;
+      this.search();
+    }
+  
+    search() {
+      console.log(`searching for ${this.query}`);
+    }
+  }
+
+  const searchBar = new SearchBar();
+
+const queryForHelloWorld = () => {
+  searchBar.type("h");
+  searchBar.type("e");
+  searchBar.type("l");
+  searchBar.type("l");
+  searchBar.type("o");
+  searchBar.type(" ");
+  searchBar.type("w");
+  searchBar.type("o");
+  searchBar.type("r");
+  searchBar.type("l");
+  searchBar.type("d");
+};
+
+searchBar.search = searchBar.search.myDebounce(500);
+
+queryForHelloWorld();
+
+
